634d411276e81408c02ee632e3e4875fcc4a3e3d
Elise van Iterson <elisevaniterson@Elises-MacBook-Pro.local>
2025-01-24 15:31:58 +0100

    add css

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ API_KEY = os.getenv('API_KEY')
 HISTORY_API_URL = 'https://api.api-ninjas.com/v1/historicalevents'
 
 # Initialize Flask app with the custom template folder path
-app = Flask(__name__, template_folder=os.path.join(os.path.dirname(__file__), 'app', 'templates'))
+app = Flask(__name__)
 
 @app.route('/')
 def index():
@@ -16,38 +16,38 @@ def index():
 
 @app.route('/history', methods=['GET'])
 def history():
-    year = request.args.get('year')
+    # year = request.args.get('year')
     
-    if not year:
-        return jsonify({"error": "Year is required"}), 400
+    # if not year:
+    #     return jsonify({"error": "Year is required"}), 400
 
-    api_url = f'{HISTORY_API_URL}?year={year}'
+    # api_url = f'{HISTORY_API_URL}?year={year}'
 
-    print("API URL:", api_url)
+    # print("API URL:", api_url)
 
-    response = requests.get(api_url, headers={'X-Api-Key': API_KEY})
+    # response = requests.get(api_url, headers={'X-Api-Key': API_KEY})
     
-    print("Status Code:", response.status_code)
-    print("Response Text:", response.text)
+    # print("Status Code:", response.status_code)
+    # print("Response Text:", response.text)
 
-    if response.status_code == 200:
-        return jsonify(response.json())
-    else:
-        return jsonify({"error": "Error fetching data"}), 500
+    # if response.status_code == 200:
+    #     return jsonify(response.json())
+    # else:
+    #     return jsonify({"error": "Error fetching data"}), 500
 
-    # response_data = [
-    # {
-    #     "year": "-45",
-    #     "month": "01",
-    #     "day": "01",
-    #     "event": "The Julian calendar takes effect..."
-    # },
-    # # ... more events
-    # ]
-    # return jsonify(response_data)
+    response_data = [
+    {
+        "year": "-45",
+        "month": "01",
+        "day": "01",
+        "event": "The Julian calendar takes effect..."
+    },
+    # ... more events
+    ]
+    return jsonify(response_data)
 
 # Get the port from the environment (Heroku provides a dynamic port, fallback to 5000 for local development)
-port = int(os.environ.get('PORT', 5000))
+port = int(os.environ.get('PORT', 8080))
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=port)
+    app.run(host='0.0.0.0', port=port, debug=True)
