4d94a2fd96fc7363b7fe7f55910faab12e423b4a
Elise van Iterson <elisevaniterson@Elises-MacBook-Pro.local>
2025-01-24 15:33:41 +0100

    add front end css ready to deploy

--- app.py
+++ app.py
@@ -16,38 +16,25 @@ def index():
 
 @app.route('/history', methods=['GET'])
 def history():
-    # year = request.args.get('year')
+    year = request.args.get('year')
     
-    # if not year:
-    #     return jsonify({"error": "Year is required"}), 400
+    if not year:
+        return jsonify({"error": "Year is required"}), 400
 
-    # api_url = f'{HISTORY_API_URL}?year={year}'
+    api_url = f'{HISTORY_API_URL}?year={year}'
 
-    # print("API URL:", api_url)
+    print("API URL:", api_url)
 
-    # response = requests.get(api_url, headers={'X-Api-Key': API_KEY})
+    response = requests.get(api_url, headers={'X-Api-Key': API_KEY})
     
-    # print("Status Code:", response.status_code)
-    # print("Response Text:", response.text)
+    if response.status_code == 200:
+        return jsonify(response.json())
+    else:
+        return jsonify({"error": "Error fetching data"}), 500
 
-    # if response.status_code == 200:
-    #     return jsonify(response.json())
-    # else:
-    #     return jsonify({"error": "Error fetching data"}), 500
-
-    response_data = [
-    {
-        "year": "-45",
-        "month": "01",
-        "day": "01",
-        "event": "The Julian calendar takes effect..."
-    },
-    # ... more events
-    ]
-    return jsonify(response_data)
 
 # Get the port from the environment (Heroku provides a dynamic port, fallback to 5000 for local development)
 port = int(os.environ.get('PORT', 8080))
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=port, debug=True)
+    app.run(host='0.0.0.0', port=port, debug=False)
